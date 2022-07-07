cf4048d988339bd1f9f072bacbeec63f6a6ec0b1
Jai <jaime.lapiz@usjr.edu.ph>
2022-07-07 18:38:06 +0800

    added note model and implemented fetching of single note info

--- lib/services/notes_service.dart
+++ lib/services/notes_service.dart
@@ -4,6 +4,8 @@ import 'package:rest_api/models/api_response.dart';
 import 'package:rest_api/models/note_for_listing.dart';
 import 'package:http/http.dart' as http;
 
+import '../models/note.dart';
+
 class NotesService {
   static const API = 'https://tq-notes-api-jkrgrdggbq-el.a.run.app';
   static const headers = {'apiKey': '9d57e8aa-d3ee-47e2-9a58-654a60a92659'};
@@ -14,15 +16,7 @@ class NotesService {
         final jsonData = json.decode(data.body);
         final notes = <NoteForListing>[];
         for (var item in jsonData) {
-          final note = NoteForListing(
-            noteID: item['noteID'],
-            noteTitle: item['noteTitle'],
-            createDateTime: DateTime.parse(item['createDateTime']),
-            latestEditDateTime: item['latestEditDateTime'] != null
-                ? DateTime.parse(item['latestEditDateTime'])
-                : null,
-          );
-          notes.add(note);
+          notes.add(NoteForListing.fromJson(item));
         }
         return APIResponse<List<NoteForListing>>(data: notes);
       }
@@ -30,5 +24,19 @@ class NotesService {
           error: true, errorMessage: 'An error occured');
     }).catchError((_) => APIResponse<List<NoteForListing>>(
         error: true, errorMessage: 'An error occured'));
+  }
+
+  Future<APIResponse<Note>> getNote(String noteID) {
+    return http
+        .get(Uri.parse(API + '/notes/' + noteID), headers: headers)
+        .then((data) {
+      if (data.statusCode == 200) {
+        final jsonData = json.decode(data.body);
+
+        return APIResponse<Note>(data: Note.fromJson(jsonData));
+      }
+      return APIResponse<Note>(error: true, errorMessage: 'An error occured');
+    }).catchError((_) =>
+            APIResponse<Note>(error: true, errorMessage: 'An error occured'));
   }
 }
